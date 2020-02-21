da6029eea44ef4e4d12200bd8e5a8560627d52bc
aksam432 <akshaykrishnaak9@gmail.com>
2020-02-21 17:12:53 +0100

    Added necessay details to setup.py & created MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ import sys
 
 if sys.version_info[:2] < (3,0):
     raise SystemExit('Python 3 required')
-
 setup(
-        name='ccsfit',
+        name='ccs',
         version="0.1.0",
         url="https://github.com/aksam432/CCS",
         description=" A Python package for fitting two-body potentials using curvature constrained splines",
         author="Akshay Krishna AK",
         author_email="akshay.kandy@kemi.uu.se",
+        platforms=['unix'],
+        include_package_data=True,
+        packages=find_packages(),
+        license='GPLv3',
+        scripts=['bin/atom-json','bin/ccs_fit'],
         install_requires=[
             "cvxopt==1.2.4",
             "matplotlib==3.0.3",
@@ -24,4 +28,3 @@ setup(
         keywords = ['Two-body Potentials', 'optimisation', 'Force Field'],
         classifiers=[],
         )
-
