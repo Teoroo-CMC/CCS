eb2a7b2846a3a0a62c56ee19657f37d83ad0922d
aksam432 <akshaykrishnaak9@gmail.com>
2020-04-24 01:28:11 +0200

    Added dftb spline functions

--- ccs/spline_functions.py
+++ ccs/spline_functions.py
@@ -5,6 +5,7 @@ import logging
 
 import numpy as np
 import scipy.linalg as linalg
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
 
@@ -88,11 +89,38 @@ def spline_eval012(a, b, c, d, r, Rcut, Rmin, dx, x):
             (b[index-1] + dr*(0.5*c[index-1] + (d[index-1]*dr/3.0)))
         f1 = b[index - 1] + dr * (c[index - 1] + (0.5 * d[index - 1] * dr))
         f2 = c[index - 1] + d[index - 1] * dr
-        print('value of f0' + str(f0))
-        return f0, f1, f2
+#        print( type(f0))
+        return float(f0), float(f1), float(f2)
     else:
         raise ValueError(' r < Rmin')
+    
+def get_spline_coeffs(xx, yy, deriv0, deriv1):
+    'Spline coefficients for a spline with given 1st derivatives at its ends.'
 
+    nn = len(xx)
+    kk = xx[1:] - xx[:-1]
+    mu = kk[:-1] / (kk[:-1] + kk[1:])
+    mun = 1.0
+    alpha = 1.0 - mu
+    alpha0 = 1.0
+    mu = np.hstack((mu, [ mun ]))
+    alpha = np.hstack(([ alpha0 ], alpha))
+    dd = 6.0 / (kk[:-1] + kk[1:]) * ((yy[2:] - yy[1:-1]) / kk[1:] 
+                                     - (yy[1:-1] - yy[0:-2]) / kk[:-1])
+    d0 = 6.0 / kk[0] * ((yy[1] - yy[0]) / kk[0] - deriv0)
+    dn = 6.0 / kk[-1] * (deriv1 - (yy[-1] - yy[-2]) / kk[-1])
+    dd = np.hstack((d0, dd, dn))
+    mtx = 2.0 * np.identity(nn, dtype=float)
+    for ii in range(nn - 1):
+        mtx[ii,ii+1] = alpha[ii]
+        mtx[ii+1,ii] = mu[ii]
+    mm = linalg.solve(mtx, dd)
+    c0 = yy[:-1]
+    c1 = (yy[1:] - yy[:-1]) / kk - (2.0 * mm[:-1] + mm[1:]) / 6.0 * kk
+    c2 = mm[:-1] / 2.0
+    c3 = (mm[1:] - mm[:-1]) / (6.0 * kk)
+    mtx = np.array([ c0, c1, c2, c3 ])
+    return np.transpose(mtx)
 
 def spline_energy_model(Rcut, Rmin, df, cols, dx, size, x):
     """ Constructs the v matrix 
@@ -170,7 +198,65 @@ def write_error(mdl_eng, ref_eng, mse, fname='error.out'):
                header=header,
                footer=footer,
                fmt="%-15.5f")
+def get_expcoeffs(aa, bb, cc , r0):
+    alpha = -cc / bb
+    beta = alpha * r0 + np.log(cc / alpha ** 2)
+    gamma = aa - np.exp(-alpha * r0 + beta)
+    return alpha, beta, gamma
 
+
+def get_exp_values(coeffs, rr):
+    aa, bb, cc = coeffs
+    return np.exp(-aa * rr + bb) + cc
+
+def write_as_nxy(fname, datadesc, vectors, column_names):
+    header_parts = []
+    for ii, colname in enumerate(column_names):
+        header_parts.append('Column {}: {}'.format(ii + 1, colname))
+    header = '\n'.join(header_parts)
+    data = np.array(vectors).transpose()
+    np.savetxt(fname, data, header=header)
+    print_io_log(fname, datadesc)
+
+
+def print_io_log(fname, fcontent):
+    print("{} -> '{}'".format(fcontent, fname))
+
+
+def write_splinerep(fname, expcoeffs, splcoeffs, rr, rcut):
+    delta=0.01
+    fp = open(fname,'w')
+    fp.write('Spline\n')
+    fp.write('{:d} {:4.3f}\n'.format(len(rr),rcut+delta))
+    print(*expcoeffs)
+    fp.write('{:15.8E} {:15.8E} {:15.8E}\n'.format(*expcoeffs))
+    
+    splcoeffs_format = ' '.join(['{:6.3f}'] * 2 + ['{:15.8E}'] * 4) + '\n'
+    for ir in range(len(rr) - 1):
+        rcur = rr[ir]
+        rnext = rr[ir + 1]
+        fp.write(splcoeffs_format.format(rcur, rnext, *splcoeffs[ir]))
+    poly5coeffs_format = ' '.join(['{:6.3f}'] * 2 + ['{:15.8E}'] * 6) + '\n'
+    fp.write(poly5coeffs_format.format(rr[-1],rr[-1]+delta, 0.0, 0.0, 0.0,0.0,0.0,0.0))
+    fp.close()
+    print_io_log(fname, 'Repulsive in Spline format')
+
+def append_spline (fin, fspl, fout):
+    """Take electronic part from fin add fspl and write to fout."""
+    with open(fspl, 'r') as f:
+        spline = f.readlines()
+    with open(fin, 'r') as f:
+        skf = f.readlines()
+    newskf = []
+    for line in skf:
+        if line == SPLINETAG:
+            break
+        else:
+            newskf.append(line)
+    with open(fout, 'w') as f:
+        f.writelines(newskf)
+        f.write('\n')         # do we need this line?
+        f.writelines(spline)  # assuming fspl already has SPLINETAG
 
 class Twobody():
     """ Twobody class describes properties of an Atom pair"""
