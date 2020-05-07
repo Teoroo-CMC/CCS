e7bbec5b9718a11bb07c93d0f5ab3f672c7829d3
aksam432 <akshaykrishnaak9@gmail.com>
2020-05-07 14:39:37 +0200

    MULTIDFTB: objective- compatability with multiple elements

--- ccs/objective.py
+++ ccs/objective.py
@@ -1,5 +1,6 @@
 """ This module constructs and solves the spline objective"""
 import logging
+import itertools
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -38,7 +39,7 @@ class Objective():
         self.NP = len(l_twb)
         self.cparams = [self.l_twb[i].cols for i in range(self.NP)]
         self.ns = len(ref_E)
-        logger.debug(" The reference energy : \n %s", self.ref_E)
+        logger.debug(" The reference energy : \n %s \n Number of pairs:%s", self.ref_E,self.NP)
 
     @staticmethod
     def solver(P, q, G, h, MAXITER=300, tol=(1e-10, 1e-10, 1e-10)):
@@ -74,7 +75,7 @@ class Objective():
         """
         return np.format_float_scientific(np.sum((self.ref_E - (np.ravel(self.M.dot(x))))**2)/self.ns, precision=4)
 
-    def plot(self, E_model, s_interval, s_a, x):
+    def plot(self, name, E_model, s_interval, Dismat,s_a, x):
         """ function to plot the results
         
         Args:
@@ -106,18 +107,52 @@ class Objective():
         ax3.set_xlabel('Distance')
 
         ax4 = fig.add_subplot(2, 2, 4)
-        n, bins, patches = plt.hist(x=np.ravel(
-            self.l_twb[0].Dismat), bins=self.l_twb[0].interval, color='g', rwidth=0.85)
+        n, bins, patches = plt.hist(x=np.ravel(Dismat), bins=s_interval, color='g', rwidth=0.85)
         ax4.set_ylabel('Frequency of a distance')
         ax4.set_xlabel('Spline interval')
         plt.tight_layout()
-        plt.savefig('summary.png')
+        plt.savefig(name+'-summary.png')
         plt.show()
+        
+    def get_coeffs(self,x,E_model):
+        temp=0
+        for i in range(self.NP):
+            curvatures= x[temp : temp+self.cparams[i]]
+            temp=self.cparams[i]
+            s_a = np.dot(self.l_twb[i].A, curvatures)
+            s_b = np.dot(self.l_twb[i].B, curvatures)
+            s_c = np.dot(self.l_twb[i].C, curvatures)
+            s_d = np.dot(self.l_twb[i].D, curvatures)
+
+            
+    #        splcoeffs = np.hstack((s_a, s_b, s_c, s_d))
+            splderivs = sf.spline_eval012(s_a,s_b,s_c,s_d,self.l_twb[i].Rmin,self.l_twb[i].Rcut,self.l_twb[i].Rmin,self.l_twb[i].dx,self.l_twb[i].interval)
+            expcoeffs= sf.get_expcoeffs(*splderivs,self.l_twb[i].Rmin)
+            print (expcoeffs)
+            print (type(expcoeffs))
+            expbuf=0.5
+            rexp = np.linspace( self.l_twb[i].Rmin -expbuf, self.l_twb[i].Rmin, int(expbuf/self.l_twb[i].dx)+1)
+            expvals = sf.get_exp_values(expcoeffs,rexp)
+            sf.write_as_nxy('headfit.dat', 'Exponentail head', (rexp, expvals), ('rr', 'exponential head'))
+            s_a = np.insert(s_a,0,splderivs[i])
+            splcoeffs = sf.get_spline_coeffs(self.l_twb[i].interval,s_a,splderivs[1],0)
+            sf.write_splinerep(self.l_twb[i].name+"repulsive.dat", np.array(expcoeffs).tolist(), splcoeffs, self.l_twb[i].interval,self.l_twb[i].Rcut)
+    #   #     print (type(splcoeffs))
+        #    sf.write_splinecoeffs(self.l_twb[0], splcoeffs)
+            self.plot(self.l_twb[i].name, E_model, self.l_twb[i].interval,self.l_twb[i].Dismat, s_a, s_c)
+    
+    def list_iterator(self):
+        tmp = [range(i+1) for i in self.cparams]
+       
+        N_list = list(itertools.product(*tmp))
+        return N_list
+        
 
     def solution(self):
         """ Function to solve the objective with constraints
         """
         self.M = self.get_M()
+        logger.debug("\n Shape of M matrix is : %s",self.M.shape)
         P = matrix(np.transpose(self.M).dot(self.M))
         eigvals = np.linalg.eigvals(P)
         logger.debug("Eigenvalues:%s",eigvals)
@@ -126,35 +161,46 @@ class Objective():
         print(np.all((eigvals >0)))
         q = -1*matrix(np.transpose(self.M).dot(self.ref_E))
         N_switch_id = 0
-        obj = np.zeros(self.l_twb[0].cols+1)
+        Nswitch_list = self.list_iterator()
+       # print(Nswitch_list)
+        obj = np.zeros(([x+1 for x in self.cparams]))
         sol_list = []
         if self.l_twb[0].Nswitch is None:
-            for count, N_switch_id in enumerate(range(self.l_twb[0].Nknots+2)):
+            for N_switch_id in Nswitch_list:
                 G = self.get_G(N_switch_id)
+                print(N_switch_id)
                 logger.debug(
-                    "\n Nswitch_id : %d and G matrix:\n %s", N_switch_id, G)
+                    "\n Nswitch_id : %s and G matrix:\n %s", N_switch_id, G)
                 h = np.zeros(G.shape[1])
                 sol = self.solver(P, q, matrix(G), matrix(h))
-                obj[count] = self.eval_obj(sol['x'])
+                obj[N_switch_id] = self.eval_obj(sol['x'])
+                print(obj[N_switch_id])
+                print(sol['x'])
                 sol_list.append(sol)
+                
 
             mse = np.min(obj)
+            print (obj)
             opt_sol_index = np.ravel(np.argwhere(obj == mse))
-            logger.info("\n The best switch is : %d", opt_sol_index)
-            opt_sol = sol_list[opt_sol_index[0]]
+            logger.info("\n The best switch is : %s and mse : %s", opt_sol_index,mse)
+            
+            G_opt = self.get_G(opt_sol_index)
+            opt_sol = self.solver(P, q, matrix(G_opt), matrix(h))
+            print(opt_sol['x'])
+            print(self.eval_obj(opt_sol['x']))
+
 
         else:
-            N_switch_id = self.l_twb[0].Nswitch
+            N_switch_id = [self.l_twb[i].Nswitch for i in range(self.NP)]
             G = self.get_G(N_switch_id)
             logger.debug(
                     "\n Nswitch_id : %d and G matrix:\n %s", N_switch_id, G)
             h = np.zeros(G.shape[1])
             opt_sol = self.solver(P, q, matrix(G), matrix(h))
             mse = float(self.eval_obj(opt_sol['x']))
-            logger.debug(
-                    "\n mse: %s \n",mse)
-
+            logger.info("\n Nknots: %s and mse : %s", N_switch_id,mse)
         x = np.array(opt_sol['x'])
+        print(x)
         E_model=np.ravel(self.M.dot(x))
         curvatures = x[0:self.cparams[0]]
         epsilon = x[-self.cols_sto:]
@@ -162,28 +208,9 @@ class Objective():
         logger.info("\n The optimal solution is : \n %s", x)
         logger.info("\n The optimal curvatures are:\n%s\nepsilon:%s",
                     curvatures, epsilon)
+        self.get_coeffs(list(x),E_model)
 #
-        s_a = np.dot(self.l_twb[0].A, curvatures)
-        s_b = np.dot(self.l_twb[0].B, curvatures)
-        s_c = np.dot(self.l_twb[0].C, curvatures)
-        s_d = np.dot(self.l_twb[0].D, curvatures)
-
         sf.write_error(E_model, self.ref_E, mse)
-#        splcoeffs = np.hstack((s_a, s_b, s_c, s_d))
-        splderivs = sf.spline_eval012(s_a,s_b,s_c,s_d,self.l_twb[0].Rmin,self.l_twb[0].Rcut,self.l_twb[0].Rmin,self.l_twb[0].dx,self.l_twb[0].interval)
-        expcoeffs= sf.get_expcoeffs(*splderivs,self.l_twb[0].Rmin)
-        print (expcoeffs)
-        print (type(expcoeffs))
-        expbuf=0.5
-        rexp = np.linspace( self.l_twb[0].Rmin -expbuf, self.l_twb[0].Rmin, int(expbuf/self.l_twb[0].dx)+1)
-        expvals = sf.get_exp_values(expcoeffs,rexp)
-        sf.write_as_nxy('headfit.dat', 'Exponentail head', (rexp, expvals), ('rr', 'exponential head'))
-        s_a = np.insert(s_a,0,splderivs[0])
-        splcoeffs = sf.get_spline_coeffs(self.l_twb[0].interval,s_a,splderivs[1],0)
-        sf.write_splinerep("repulsive.dat", np.array(expcoeffs).tolist(), splcoeffs, self.l_twb[0].interval,self.l_twb[0].Rcut)
-#   #     print (type(splcoeffs))
-    #    sf.write_splinecoeffs(self.l_twb[0], splcoeffs)
-        self.plot(E_model, self.l_twb[0].interval, s_a, s_c)
         return E_model
 
 
@@ -221,15 +248,16 @@ class Objective():
         Returns:
             ndarray: returns G matrix
         """
-        g = block_diag(-1*np.identity(n_switch),
-                       np.identity(self.l_twb[0].cols-n_switch))
+        g = block_diag(-1*np.identity(n_switch[0]),
+                       np.identity(self.l_twb[0].cols-n_switch[0]))
         logger.debug("\n g matrix:\n%s", g)
         if self.NP == 1:
             G = block_diag(g, np.zeros(self.cols_sto))
             return G
         else:
             for elem in range(1, self.NP):
-                tmp_G = block_diag(g, -1*np.identity(self.l_twb[elem].cols))
+                tmp_G = block_diag(g, -1*np.identity(n_switch[elem]),
+                       np.identity(self.l_twb[elem].cols-n_switch[elem]))
                 g = tmp_G
-        G = block_diag(g, self.cols_sto)
+        G = block_diag(g, np.zeros(self.cols_sto))
         return G
