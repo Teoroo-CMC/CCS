23a78ec215dd6dba7b748168c79e430fa940d6ec
Thijs Smolders <smolders.thijs@gmail.com>
2022-10-14 14:56:30 +0200

    build: moved bin python scripts to __init__.py

--- src/ccs/__init__.py
+++ src/ccs/__init__.py
@@ -10,3 +10,8 @@ from importlib.metadata import version
 __version__ = version("ccs")
 
 from ccs.ase_calculator import CCS
+from ccs.scripts import ccs_build_db
+from ccs.scripts import ccs_export_sktable
+from ccs.scripts import ccs_fetch
+from ccs.scripts import ccs_fit
+from ccs.scripts import ccs_validate
