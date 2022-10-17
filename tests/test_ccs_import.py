1ecb1b2d72d8ecd0ddf82db7899274c53e66a3cd
Thijs Smolders <smolders.thijs@gmail.com>
2022-10-17 13:34:33 +0200

    fix: fixed testing routine

--- tests/test_ccs_import.py
+++ tests/test_ccs_import.py
@@ -1,3 +1,4 @@
 import ccs
 
-assert ccs.__version__=="0.1.0", "CCS did not import, or version has changed!"
+def test_import():
+    assert ccs.__version__=="0.1.0", "CCS did not import, or version has changed!"
