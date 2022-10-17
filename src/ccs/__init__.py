18975a05bb31178353b3a367ce6278c08347a303
Thijs Smolders <smolders.thijs@gmail.com>
2022-10-17 08:19:36 +0200

    fix: fixed automatic loading of the original functions in the bin/ folder

--- src/ccs/__init__.py
+++ src/ccs/__init__.py
@@ -10,8 +10,8 @@ from importlib.metadata import version
 __version__ = version("ccs")
 
 from ccs.ase_calculator.ccs_ase_calculator import CCS
-from ccs.scripts import ccs_build_db
-from ccs.scripts import ccs_export_sktable
-from ccs.scripts import ccs_fetch
-from ccs.scripts import ccs_fit
-from ccs.scripts import ccs_validate
+from ccs.scripts.ccs_build_db import main as ccs_build_db
+from ccs.scripts.ccs_export_sktable import main as ccs_export_sktable
+from ccs.scripts.ccs_fetch import main as ccs_fetch
+from ccs.scripts.ccs_fit import main as ccs_fit
+from ccs.scripts.ccs_validate import main as ccs_validate
