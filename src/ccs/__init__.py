dcf11aa3f11f1d2564f13e0f93aa84deeb9073a7
Thijs Smolders <smolders.thijs@gmail.com>
2022-10-31 10:24:05 +0100

    refactor: using black to format python code to adhere to PEP8

--- src/ccs/__init__.py
+++ src/ccs/__init__.py
@@ -1,12 +1,13 @@
-#------------------------------------------------------------------------------#
+# ------------------------------------------------------------------------------#
 #  CCS: Curvature Constrained Splines                                          #
 #  Copyright (C) 2019 - 2021  CCS developers group                             #
 #                                                                              #
 #  See the LICENSE file for terms of usage and distribution.                   #
-#------------------------------------------------------------------------------#
+# ------------------------------------------------------------------------------#
 
 # read version from installed package
 from importlib.metadata import version
+
 __version__ = version("ccs")
 
 from ccs.ase_calculator.ccs_ase_calculator import CCS
