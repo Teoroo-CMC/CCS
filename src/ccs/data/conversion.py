fb8d3c9a0f03c10ad93c9549ded77d4a9555f61e
Thijs Smolders <smolders.thijs@gmail.com>
2022-11-08 15:12:01 +0100

    refactor(conversion.py): changed manual conversion units to scipy standards

--- src/ccs/data/conversion.py
+++ src/ccs/data/conversion.py
@@ -9,13 +9,14 @@
 Conversion factory required by the CCS project.
 """
 
+from scipy.constants import value, angstrom
 
 # Bohr --> Angstrom
-Bohr__AA = 0.529177249
+Bohr__AA = value('Bohr radius')/angstrom  # 0.529177249 
 # Angstrom --> Bohr
 AA__Bohr = 1.0 / Bohr__AA
 
 # Hartree --> eV
-Hartree__eV = 27.2113845
+Hartree__eV = value('hartree-electron volt relationship') # 27.2113845
 # eV --> Hartree
 eV__Hartree = 1.0 / Hartree__eV
