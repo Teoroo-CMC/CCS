0ef6336d50515f50397c539a76839e784722f2ab
Thijs Smolders <smolders.thijs@gmail.com>
2022-11-08 10:09:06 +0100

    Redone changes proposed by vanderhe; branch 'updateCopyrightAndCleanup'

--- src/ccs/regression_tool/regression.py
+++ src/ccs/regression_tool/regression.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+#------------------------------------------------------------------------------#
+#  CCS: Curvature Constrained Splines                                          #
+#  Copyright (C) 2019 - 2022  CCS developers group                             #
+#                                                                              #
+#  See the LICENSE file for terms of usage and distribution.                   #
+#------------------------------------------------------------------------------#
+
 import logging
 import itertools
 import pickle
