cd0dc12bf7f72f4b2aa28980edf4bbb0a2c18e88
Thijs Smolders <smolders.thijs@gmail.com>
2022-11-08 11:30:44 +0100

    build: putting back __init__.py

---
+++ src/ccs/__init__.py
@@ -0,0 +1,21 @@
+# ------------------------------------------------------------------------------#
+#  CCS: Curvature Constrained Splines                                          #
+#  Copyright (C) 2019 - 2022  CCS developers group                             #
+#                                                                              #
+#  See the LICENSE file for terms of usage and distribution.                   #
+# ------------------------------------------------------------------------------#
+
+# read version from installed package
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("ccs")
+except PackageNotFoundError:
+    __version__ = "unknown version"
+
+from ccs.ase_calculator.ccs_ase_calculator import CCS
+from ccs.scripts.ccs_build_db import main as ccs_build_db
+from ccs.scripts.ccs_export_sktable import main as ccs_export_sktable
+from ccs.scripts.ccs_fetch import main as ccs_fetch
+from ccs.scripts.ccs_fit import main as ccs_fit
+from ccs.scripts.ccs_validate import main as ccs_validate
