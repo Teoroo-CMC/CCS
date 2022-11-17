c6104555ef5dfb53c1653d58475aa4841a9fd484
Thijs Smolders <smolders.thijs@gmail.com>
2022-11-17 15:00:28 +0100

    fix(__init__.py): fixed import statements

--- src/ccs/__init__.py
+++ src/ccs/__init__.py
@@ -10,9 +10,9 @@ from importlib.metadata import version
 
 __version__ = version("ccs")
 
-from ccs.ase_calculator.ccs_ase_calculator import CCS
-from ccs.scripts.ccs_build_db import main as ccs_build_db
-from ccs.scripts.ccs_export_sktable import main as ccs_export_sktable
-from ccs.scripts.ccs_fetch import main as ccs_fetch
-from ccs.scripts.ccs_fit import main as ccs_fit
-from ccs.scripts.ccs_validate import main as ccs_validate
+# from ccs.ase_calculator.ccs_ase_calculator import CCS
+# from ccs.scripts.ccs_build_db import main as ccs_build_db
+# from ccs.scripts.ccs_export_sktable import main as ccs_export_sktable
+from ccs.scripts.ccs_fetch import ccs_fetch as ccs_fetch
+from ccs.fitting.main import twp_fit as ccs_fit
+# from ccs.scripts.ccs_validate import main as ccs_validate
