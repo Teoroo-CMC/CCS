73d1e0771d7340732cd38e331a94c7e5dce4f32e
Alexei Torgashov <alexei.torgashov@yahoo.de>
2024-03-04 22:26:00 +0100

    format ccs_export_sktable and remove unused variables

--- src/ccs_fit/scripts/ccs_export_sktable.py
+++ src/ccs_fit/scripts/ccs_export_sktable.py
@@ -1,31 +1,24 @@
 import sys
 import os
 import numpy as np
-import itertools as it
-from collections import OrderedDict, defaultdict
-from numpy import linalg as LA
 import json
 from ase.units import Bohr, Hartree
 
 
-def _write(elem1, elem2, CCS_params, exp=True):
+def _write(elem1, elem2, CCS_params):
     elem1 = elem1
     elem2 = elem2
     no_pair = False
     try:
         pair = elem1 + "-" + elem2
-        rcut = CCS_params["Two_body"][pair]["r_cut"]
     except:
         try:
             pair = elem2 + "-" + elem1
-            rcut = CCS_params["Two_body"][pair]["r_cut"]
         except:
-            rcut = 0.0
             no_pair = True
     if no_pair:
         pass
     else:
-        Rmin = CCS_params["Two_body"][pair]["r_min"] / Bohr
         Rcut = CCS_params["Two_body"][pair]["r_cut"] / Bohr
         a = CCS_params["Two_body"][pair]["spl_a"]
         b = CCS_params["Two_body"][pair]["spl_b"]
@@ -34,7 +27,6 @@ def _write(elem1, elem2, CCS_params, exp=True):
         aa = CCS_params["Two_body"][pair]["exp_a"]
         bb = CCS_params["Two_body"][pair]["exp_b"]
         cc = CCS_params["Two_body"][pair]["exp_c"]
-        exp = False
         x = CCS_params["Two_body"][pair]["r"]
         dx = CCS_params["Two_body"][pair]["dr"]
 
@@ -46,7 +38,7 @@ def _write(elem1, elem2, CCS_params, exp=True):
                 if i < (len(x) - 1):
                     print(
                         x[i] / Bohr,
-                        (x[i+1]) / Bohr,
+                        (x[i + 1]) / Bohr,
                         a[i] / Hartree,
                         b[i] * (Bohr) / Hartree,
                         c[i] * (Bohr**2) / Hartree,
@@ -73,7 +65,7 @@ def _write(elem1, elem2, CCS_params, exp=True):
                 if i < (len(x) - 1):
                     print(
                         x[i] / Bohr,
-                        (x[i+1]) / Bohr,
+                        (x[i + 1]) / Bohr,
                         a[i] / Hartree,
                         b[i] * (Bohr) / Hartree,
                         c[i] * (Bohr**2) / Hartree,
@@ -94,29 +86,28 @@ def _write(elem1, elem2, CCS_params, exp=True):
 
 
 def write_dftb_spline(CCS_params_file):
-
     with open(CCS_params_file, "r") as f:
         CCS_params = json.load(f)
 
     for pair in CCS_params["Two_body"]:
         elem = pair.split("-")
-        _write(elem[0], elem[1], CCS_params, exp=True)
+        _write(elem[0], elem[1], CCS_params)
 
 
 def main():
-
     size = os.get_terminal_size()
     c = size.columns
-    txt = "-"*c
+    txt = "-" * c
     print("")
     print(txt)
 
     try:
         import art
-        txt = art.text2art('CCS:export sk-table')
+
+        txt = art.text2art("CCS:export sk-table")
         print(txt)
     except:
-        print('CCS: export sk-table')
+        print("CCS: export sk-table")
 
     try:
         CCS_params_file = sys.argv[1]
@@ -126,7 +117,7 @@ def main():
 
     size = os.get_terminal_size()
     c = size.columns
-    txt = "-"*c
+    txt = "-" * c
     print(txt)
     print("")
 
